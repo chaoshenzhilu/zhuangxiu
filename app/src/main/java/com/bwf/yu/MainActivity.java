b129b8f7862c42be368def9692274140f762946a
chaoshenzhilu <790166359@qq.com>
2017-01-03 18:59:05 +0800

    Initial commit

---
+++ app/src/main/java/com/bwf/yu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bwf.yu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
